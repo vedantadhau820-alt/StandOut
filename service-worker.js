c07bdc1dd9fa53d97a968a88a073df55b2b0a981
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-01-05 00:42:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,36 @@ const urlsToCache = [
   "/m4.mp3",
   "/m5.mp3",
   "/m6.mp3",
-  "/Images",
+  "/Images/e1.jpg",
+  "/Images/e2.jpg",
+  "/Images/e3.jpg",
+  "/Images/e4.jpg",
+  "/Images/e5.jpg",
+  "/Images/e6.jpg",
+  "/Images/e7.jpg",
+  "/Images/e8.jpg",
+  "/Images/d1.jpg",
+  "/Images/d2.jpg",
+  "/Images/d3.jpg",
+  "/Images/d4.jpg",
+  "/Images/d5.jpg",
+  "/Images/d6.jpg",
+  "/Images/d7.jpg",
+  "/Images/c1.jpg",
+  "/Images/c2.jpg",
+  "/Images/c3.jpg",
+  "/Images/c4.jpg",
+  "/Images/c5.jpg",
+  "/Images/b1.jpg",
+  "/Images/b2.jpg",
+  "/Images/b3.jpg",
+  "/Images/b4.jpg",
+  "/Images/b5.jpg",
+  "/Images/a1.jpg",
+  "/Images/a2.jpg",
+  "/Images/a3.jpg",
+  "/Images/s1.jpg",
+  "/Images/s2.jpg",
 ];
 
 self.addEventListener("install", event => {
@@ -38,4 +67,5 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
 
