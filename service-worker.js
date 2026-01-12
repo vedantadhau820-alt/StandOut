f7952031e5ea28192d1cc667ae495212628f72fa
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-01-13 02:57:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,43 @@ const APP_SHELL = [
   // Assets
   "/icon.jpeg",
   "/Music/Complete.mp3",
-  "/Achievements.mp3"
+  "/Music/Achievements.mp3",
+  "/Music/m1.mp3",
+  "/Music/m2.mp3",
+  "/Music/m3.mp3",
+  "/Music/m4.mp3",
+  "/Music/m5.mp3",
+  "/Music/m6.mp3",
+  "/Images/s1.jpg",
+  "/Images/s2.jpg",
+  "/Images/a1.jpg",
+  "/Images/a2.jpg",
+  "/Images/a3.jpg",
+  "/Images/b1.jpg",
+  "/Images/b2.jpg",
+  "/Images/b3.jpg",
+  "/Images/b4.jpg",
+  "/Images/b5.jpg",
+  "/Images/c1.jpg",
+  "/Images/c2.jpg",
+  "/Images/c3.jpg",
+  "/Images/c4.jpg",
+  "/Images/c5.jpg",
+  "/Images/d1.jpg",
+  "/Images/d2.jpg",
+  "/Images/d3.jpg",
+  "/Images/d4.jpg",
+  "/Images/d5.jpg",
+  "/Images/d6.jpg",
+  "/Images/d7.jpg",
+  "/Images/e1.jpg",
+  "/Images/e2.jpg",
+  "/Images/e3.jpg",
+  "/Images/e4.jpg",
+  "/Images/e5.jpg",
+  "/Images/e6.jpg",
+  "/Images/e7.jpg",
+  "/Images/e8.jpg",
 ];
 
 /* ===========================
@@ -56,33 +92,7 @@ self.addEventListener("activate", event => {
    FETCH → CACHE STRATEGY
 =========================== */
 self.addEventListener("fetch", event => {
-  const request = event.request;
-
-  // Cache images & audio dynamically
-  if (
-    request.destination === "image" ||
-    request.destination === "audio"
-  ) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(cache =>
-        cache.match(request).then(cached => {
-          if (cached) return cached;
-
-          return fetch(request).then(response => {
-            cache.put(request, response.clone());
-            return response;
-          });
-        })
-      )
-    );
-    return;
-  }
-
-  // App shell fallback
   event.respondWith(
-    caches.match(request).then(res => {
-      return res || fetch(request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
-
