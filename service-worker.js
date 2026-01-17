03be6835514162fb0f080cfacc13737837e75062
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-01-17 12:13:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "standout-v2.1.11";
+const CACHE_NAME = "standout-v2.1.12";
 //const MEDIA_CACHE = "standout-media";     // NEVER versioned
 
 const APP_SHELL = [
@@ -142,6 +142,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
